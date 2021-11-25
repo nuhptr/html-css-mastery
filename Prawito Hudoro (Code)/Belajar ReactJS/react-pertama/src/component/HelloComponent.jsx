b2ae1b7d9ca09ad659c513f26236c9a204556ca5
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2021-11-26 00:41:09 +0700

    delete

--- Prawito Hudoro (Code)/Belajar ReactJS/react-pertama/src/component/HelloComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './HelloComponent.css';
-
-const HelloFunctionalComponent = () => {
-    return <p className="text-p">Hello Functional Component</p>
-}
-
-export default HelloFunctionalComponent;
