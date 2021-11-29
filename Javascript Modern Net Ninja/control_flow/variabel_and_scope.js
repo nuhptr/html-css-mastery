c56bd7f0c4e0eb1883b8a6b9970aa92ffbbea3b1
nuhptr <nugrahaadi733@gmail.com>
2021-11-29 23:00:03 +0700

    update variabel and scope

--- Javascript Modern Net Ninja/control_flow/variabel_and_scope.js
+++ Javascript Modern Net Ninja/control_flow/variabel_and_scope.js
@@ -1,2 +1,18 @@
 // TODO: varibel & block scope
+// different scope
+let age = 30;
+let names = "Paijo";
 
+if (true) {
+  // scope inside different value with outside
+  let age = 40;
+  let names = "Shaun";
+  console.log("Inside 1st code block:", age, names); // age = 40 Shaun
+
+  if (true) {
+    let age = 50;
+    console.log("Inside 2nd code block", age);
+  }
+}
+
+console.log("outside code block:", age, names); // age = 30 Paijo
