5efb7166788b08697fe4b9be646b82fc3e927b97
nuhptr <nugrahaadi733@gmail.com>
2021-11-29 22:59:36 +0700

    update

---
+++ Javascript Modern Net Ninja/function/function_declaration.js
@@ -0,0 +1,12 @@
+// TODO: Function Declaration
+function great() {
+  console.log("Hello There");
+}
+
+// TODO: Function Expression
+const speak = function () {
+    
+}
+
+// TODO: Function Expression
+great(); // call the great function
