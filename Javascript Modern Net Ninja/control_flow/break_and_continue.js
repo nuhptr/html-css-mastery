01b12a76915dada4eb1ed3daa3e92078d806a720
nuhptr <nugrahaadi733@gmail.com>
2021-11-29 17:13:37 +0700

    update

---
+++ Javascript Modern Net Ninja/control_flow/break_and_continue.js
@@ -0,0 +1,7 @@
+// TODO: Break and Continue
+
+const scores = [50, 25, 0, 30, 100, 20, 10];
+
+for (let index = 0; index < scores.length; index++) {
+  console.log(`Your score is ${scores[0]}`)
+}
--- Javascript Modern Net Ninja/control_flow/logical_not.js
+++ Javascript Modern Net Ninja/control_flow/logical_not.js
@@ -1,1 +1,9 @@
 // TODO: Logical Not
+let user = true;
+
+if (!user) {
+    console.log(`You must logged in to continue`)
+}
+
+console.log(!user); // false
+console.log(user); // true
