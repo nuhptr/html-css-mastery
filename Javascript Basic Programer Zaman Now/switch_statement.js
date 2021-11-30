3a77107dd758d903fbfda69e8cbe48d7ee8fbb7b
nuhptr <nugrahaadi733@gmail.com>
2021-11-30 14:18:14 +0700

    switch stament

---
+++ Javascript Basic Programer Zaman Now/switch_statement.js
@@ -0,0 +1,16 @@
+let nilai = 90;
+
+switch (nilai) {
+  case 100:
+    console.info("Awesome");
+    break;
+  case 90:
+    console.info("Amazing"); // execute
+    break;
+  case 80:
+    console.info("Great");
+    break;
+  default:
+    console.info("Not a Number");
+    break;
+}
