95e5e5852430ce59dd907bab431ed44b93b64047
nuhptr <nugrahaadi733@gmail.com>
2022-07-09 20:38:17 +0700

    operator-komparasi update

--- react-developer-dicoding/dasar-pemrograman-web/javascript-and-manipulasi-dom/operator-komparasi.js
+++ react-developer-dicoding/dasar-pemrograman-web/javascript-and-manipulasi-dom/operator-komparasi.js
@@ -1,8 +1,34 @@
 let a = 10;
-let b = 20;
+let b = 10;
 
+// < kurang dari
 console.log(a < b); // true
+
+// > lebih dari
 console.log(a > b); // false
 
-// == (tidak identik)
+// == apakah kedua nilai(tidak identik)
 console.log(a == b); // false`
+
+// (!=) apakah kedua nilai tidak sama (tidak identik)
+console.log(a != b); // true
+
+// === membandingkan nilai apakah keduanya identik
+console.log(a === b); // nilainya harus sama dan identik => true
+
+// !== membandingkan kedua nilai apakah tidak identik
+console.log(a !== b); // false, karena kedua nilainya identik
+
+// >= lebih dari sama dengan
+console.log(a >= b); // true
+
+// <= kurang dari sama dengan
+console.log(a <= b); // true
+
+// study case ===
+const aString = "10";
+const aNumber = 10;
+
+console.log("-----------");
+console.log(aString === aNumber); // false
+console.log(aString == aNumber); // true -> karena keidentikannya tidak di check
