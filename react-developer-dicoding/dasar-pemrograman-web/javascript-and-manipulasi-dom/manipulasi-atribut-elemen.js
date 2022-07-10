b311c8a7906e1e718f7ed105a717b2f17606938e
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2022-07-10 13:48:54 +0700

    manipulasi and calculator location

--- react-developer-dicoding/dasar-pemrograman-web/javascript-and-manipulasi-dom/manipulasi-atribut-elemen.js
+++ react-developer-dicoding/dasar-pemrograman-web/javascript-and-manipulasi-dom/manipulasi-atribut-elemen.js
@@ -1,6 +1,20 @@
 // run in console web
-let image = document.querySelector(".catImage");
+let image;
+image = document.querySelector("#catImage");
 image.setAttribute("src", "https://i.ibb.co/55VG7vL/three-cat.jpg");
 
-let changeCaption = document.querySelector(".caption");
+let changeCaption;
+changeCaption = document.querySelector("#caption");
 changeCaption.innerHTML = "<em>Tiga anak kucing</em>";
+
+// membuat elemen baru
+let newElement;
+newElement = document.createElement("p");
+newElement.innerHTML =
+  'Anda menekan gambar kucing sebanyak <span id="count">0</span> kali';
+
+document.body.appendChild(newElement);
+
+image.addEventListener("click", function (event) {
+  document.querySelector("#count").innerText++;
+});
