8f7cae644b72db213adac69a283404b2e875d511
nuhptr <nugrahaadi733@gmail.com>
2022-07-10 13:05:01 +0700

    manipulasi atribut element

---
+++ react-developer-dicoding/dasar-pemrograman-web/javascript-and-manipulasi-dom/manipulasi-atribut-elemen.js
@@ -0,0 +1,6 @@
+// run in console web
+let image = document.querySelector(".catImage");
+image.setAttribute("src", "https://i.ibb.co/55VG7vL/three-cat.jpg");
+
+let changeCaption = document.querySelector(".caption");
+changeCaption.innerHTML = "<em>Tiga anak kucing</em>";
