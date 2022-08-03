76da97819206e666ace5095866d131bdbd6e172b
nuhptr <nugrahaadi733@gmail.com>
2022-08-03 13:53:44 +0700

    update module

---
+++ react-developer-dicoding/dasar-pemrograman-javascript/module/state.js
@@ -0,0 +1,10 @@
+const coffeStock = {
+  arabica: 100,
+  robusta: 150,
+  liberica: 200,
+};
+
+const isCoffeMachineReady = true;
+
+module.exports = isCoffeMachineReady;
+module.exports = coffeStock;
