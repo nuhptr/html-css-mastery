9926858278475a7016cd7a7a2878dc17e1b0192b
nuhptr <nugrahaadi733@gmail.com>
2022-08-04 09:50:13 +0700

    throwing-error

--- react-developer-dicoding/dasar-pemrograman-javascript/penanganan-error/throwing-error.js
+++ react-developer-dicoding/dasar-pemrograman-javascript/penanganan-error/throwing-error.js
@@ -13,16 +13,34 @@ try {
   console.log(error.name);
 }
 
-console.log('-------------')
+console.log("-------------");
 
 // Example 2
 let json2 = { age: 20 };
 
 try {
   let user = JSON.parse(JSON.stringify(json2));
-  console.log(user.name);
-  console.log(user.age);
+  console.log(user.name); // undefined
+  console.log(user.age); // 20
 } catch (error) {
   console.log(error.name);
   console.log(error.message);
+}
+
+console.log("------------");
+
+// Example 3
+let json3 = '{ "age": 20 }';
+
+try {
+  let user = JSON.parse(json3);
+
+  if (!user.name) { // kena di syntak ini
+    throw new SyntaxError("'name' is required");
+  }
+
+  console.log(user.name);
+  console.log(user.age);
+} catch (error) {
+  console.log(`JSON Error : ${error.message}`);
 }
