678cf8070bc4c04a833fcd545c96bae228e431f7
nuhptr <nugrahaadi733@gmail.com>
2022-08-06 05:50:44 +0700

    dasar javascript update

---
+++ react-developer-dicoding/front-end-web-pemula/dasar-javascript/if-else-statement.js
@@ -0,0 +1,23 @@
+const isRaining = true;
+
+console.log("Persiapan sebelum berangkat kegiatan.");
+if (isRaining) {
+  console.log("Hari ini hujan. Bawa payung.");
+}
+console.log("Berangkat kegiatan.");
+
+/* Output : 
+  Persiapan sebelum berangkat kegiatan.
+  Hari ini hujan. Bawa payung.
+  Berangkat kegiatan.
+*/
+
+let x = 50;
+
+if (x > 70) {
+  console.log(x);
+} else {
+  console.log("Nilai kurang dari 70");
+}
+
+// Output : Nilai kurang dari 70
--- react-developer-dicoding/front-end-web-pemula/dasar-javascript/operator-comparison.js
+++ react-developer-dicoding/front-end-web-pemula/dasar-javascript/operator-comparison.js
@@ -22,3 +22,11 @@ let b = 12;
 
 console.log(a < b); // true
 console.log(a > b); // false
+
+// TODO: same and identical
+const aString = "10";
+const aNumber = 10;
+
+console.log(aString == aNumber); // true, karena nilainya sama-sama 10
+console.log(aString === aNumber); // false, karena walaupun nilainya sama, tetapi tipe datanya berbeda
+
