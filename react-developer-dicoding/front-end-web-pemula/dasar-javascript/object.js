938ac2bfd28de86c9f781d865b2745d8448344a8
nuhptr <nugrahaadi733@gmail.com>
2022-08-07 14:08:03 +0700

    javascript basic update

--- react-developer-dicoding/front-end-web-pemula/dasar-javascript/object.js
+++ react-developer-dicoding/front-end-web-pemula/dasar-javascript/object.js
@@ -11,3 +11,22 @@ console.log(
 ); // Hallo, my name is Harry Meguire and my age 22
 
 console.log(`I\'m from ${user["home world"]}`); // I'm from Tattoine
+
+const spaceShip = {
+  name: "Millenium falcon",
+  manufacturer: "Corellian Engineering Corporation",
+  maxSpeed: 1200,
+  color: "Light Gray",
+};
+
+// Merubah isi nilai spaceship
+spaceShip.color = "Glossy Red";
+spaceShip["maxSpeed"] = 1300;
+
+// Tambah member baru
+spaceShip.fly = true;
+console.log(spaceShip);
+
+// Delete member
+delete spaceShip.manufacturer;
+console.log(spaceShip);
