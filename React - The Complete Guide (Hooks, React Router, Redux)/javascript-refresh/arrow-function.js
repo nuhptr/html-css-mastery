79c6cd15a6596c9a7df79090d7e4bd19d97bd709
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2022-08-25 19:38:31 +0700

    update name

--- React - The Complete Guide (Hooks, React Router, Redux)/javascript-refresh/arrow-function.js
+++
@@ -1,6 +0,0 @@
-const printMyName = (name, age) => {
-  console.log(name);
-  console.log(age);
-};
-
-printMyName("Adi", 23); // Adi <br />image.png 23
--- React - The Complete Guide (Hooks, React Router, Redux)/react-basic/react-first-project/src/components/Card.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Card(props) {
-  const classes = "card " + props.className;
-
-  return <div className={classes}>{props.children}</div>;
-}
