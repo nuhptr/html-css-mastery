b181aa036ec98feea3d0c668486dfe3a71c1ee67
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2022-10-11 10:07:52 +0700

    comment

--- javascript-complete-guide-2022/basics/assets/scripts/app.js
+++ javascript-complete-guide-2022/basics/assets/scripts/app.js
@@ -4,14 +4,50 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
+// get user input from id input-number
+const getUserNumberInput = () => {
+  return parseInt(usrInput.value);
+};
+
+// create ouput result and description
+const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber) => {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+};
+
+const add = () => {
   // TODO: Local Scope
-  currentResult += userInput.value;
-  outputResult(currentResult, "");
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+
   // Returning values
   // return result;
-}
+};
+
+const subtract = () => {
+  const enteredNumber = getUserNumberInput();
+  const intialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput("-", intialResult, enteredNumber);
+};
+
+const multiply = () => {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+};
+
+const divide = () => {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+};
 
 addBtn.addEventListener("click", add);
-
-// let calculationDescription = `(${defaultResult} + 10) * 3 / 3`;
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
