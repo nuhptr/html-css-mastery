ccf9e36fbf89ff9bb545343398bc29c4c772cdf2
nuhptr <nugrahaadi733@gmail.com>
2022-10-25 11:27:21 +0700

    dom interaction, and assignment

---
+++ javascript-complete-guide-2022/dom-assignment/assignment.js
@@ -0,0 +1,16 @@
+const task1El1 = document.getElementById("task-1");
+const task1El2 = document.querySelector("li");
+
+const h1 = document.querySelector("h1");
+const title = document.querySelector("title");
+
+task1El1.style.backgroundColor = "black";
+task1El1.style.color = "white";
+
+/* 
+task1El2.style.backgroundColor = "black";
+task1El2.style.color = "white"; 
+*/
+
+h1.textContent = "Assignment - Solved!";
+title.textContent = "Assignment - Solved!";
--- javascript-complete-guide-2022/dom-interaction/app.js
+++ javascript-complete-guide-2022/dom-interaction/app.js
@@ -1,2 +1,21 @@
-window.alert();
-console.dir(window.document);
+/* 
+  window.alert();
+  console.dir(window.document);
+ */
+
+const h1 = document.querySelector("h1");
+
+h1.textContent = "Some New Title";
+h1.style.color = "White";
+h1.style.backgroundColor = "black";
+
+const li = document.querySelector("li:last-of-type");
+li.textContent = li.textContent + " (Changed!)";
+
+const body = document.body;
+
+const listItemElements = document.getElementsByTagName("li");
+
+for (const listItemEl of listItemElements) {
+  console.dir(listItemEl);
+}
