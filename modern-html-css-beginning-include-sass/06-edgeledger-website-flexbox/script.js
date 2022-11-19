cf27fe24f32277c7bf05adbf8877ddf9daf0acf4
nuhptr <nugrahaadi733@gmail.com>
2022-11-19 14:39:35 +0700

    js contact and update edgeledger

---
+++ modern-html-css-beginning-include-sass/06-edgeledger-website-flexbox/script.js
@@ -0,0 +1,21 @@
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll backto top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other link
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
