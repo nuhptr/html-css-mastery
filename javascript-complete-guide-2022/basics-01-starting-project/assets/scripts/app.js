2f84b347a14109eb5635b3dbb66c6307cd1ddb24
nuhptr <nugrahaadi733@gmail.com>
2023-06-11 18:52:53 +0700

    refactor

--- javascript-complete-guide-2022/basics-01-starting-project/assets/scripts/app.js
+++ javascript-complete-guide-2022/basics-01-starting-project/assets/scripts/app.js
@@ -1,47 +1,79 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+// using arrays
+let logEntries = [];
 
+// get the user input
 function getUserNumberInput() {
    return parseInt(userInput.value);
 }
 
+// generate and write calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-   outputResult(currentResult, calcDescription);
+   outputResult(currentResult, calcDescription); // from vendor.js
 }
 
-// custom function
+// function writeToLog
+function writeToLog(
+   operationIdentifier,
+   prevResult,
+   number,
+   newResult
+) {
+   const logEntry = {
+      operation: operationIdentifier,
+      prevResult: prevResult,
+      number: number,
+      result: newResult,
+   };
+   logEntries.push(logEntry);
+   console.log(logEntries);
+}
+
+// function add()
 function add() {
    // parse the string to integer
    const enteredNumber = getUserNumberInput();
    const initialResult = currentResult;
-   currentResult = currentResult + enteredNumber;
+   currentResult += enteredNumber;
 
    createAndWriteOutput("+", initialResult, enteredNumber);
+
+   writeToLog("ADD", initialResult, enteredNumber, currentResult);
 }
 
+// function subtract()
 function subtract() {
    const enteredNumber = getUserNumberInput();
    const initialResult = currentResult;
-   currentResult = currentResult - enteredNumber;
+   currentResult -= enteredNumber;
 
    createAndWriteOutput("-", initialResult, enteredNumber);
+
+   writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
 }
 
+// function multiply()
 function multiply() {
    const enteredNumber = getUserNumberInput();
    const initialResult = currentResult;
-   currentResult = currentResult * enteredNumber;
+   currentResult *= enteredNumber;
 
    createAndWriteOutput("*", initialResult, enteredNumber);
+
+   writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
 }
 
+// function divide()
 function divide() {
    const enteredNumber = getUserNumberInput();
    const initialResult = currentResult;
-   currentResult = currentResult / enteredNumber;
+   currentResult /= enteredNumber;
 
    createAndWriteOutput("/", initialResult, enteredNumber);
+
+   writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
 }
 
 // alert(result); result is not defined (out of scope)
