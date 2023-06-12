54d88b64116e2c6bb3ed10de7b6f88dd1181f790
nuhptr <nugrahaadi733@gmail.com>
2023-06-12 09:41:28 +0700

    contional if else & logical AND OR

--- javascript-complete-guide-2022/basics-01-starting-project/assets/scripts/app.js
+++ javascript-complete-guide-2022/basics-01-starting-project/assets/scripts/app.js
@@ -1,84 +1,22 @@
-const defaultResult = 0;
-let currentResult = defaultResult;
-// using arrays
-let logEntries = [];
-
-// get the user input
-function getUserNumberInput() {
-   return parseInt(userInput.value);
-}
-
-// generate and write calculation log
-function writeOutputLog(operator, resultBeforeCalc, calcNumber) {
-   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-   outputResult(currentResult, calcDescription); // from vendor.js
-}
-
-// function writeToLog
-function writeToLog(operationIdentifier, prevResult, number, newResult) {
-   const logEntry = {
-      operation: operationIdentifier,
-      prevResult: prevResult,
-      number: number,
-      result: newResult,
-   };
-   logEntries.push(logEntry);
-   console.log(logEntries);
-}
-
 // function add()
 function add() {
-   // parse the string to integer
-   const enteredNumber = getUserNumberInput();
-   const initialResult = currentResult;
-   currentResult += enteredNumber;
-
-   writeOutputLog("+", initialResult, enteredNumber);
-
-   writeToLog("ADD", initialResult, enteredNumber, currentResult);
+   calculateResult("ADD");
 }
 
 // function subtract()
 function subtract() {
-   const enteredNumber = getUserNumberInput();
-   const initialResult = currentResult;
-   currentResult -= enteredNumber;
-
-   writeOutputLog("-", initialResult, enteredNumber);
-
-   writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
+   calculateResult("SUBTRACT");
 }
 
 // function multiply()
 function multiply() {
-   const enteredNumber = getUserNumberInput();
-   const initialResult = currentResult;
-   currentResult *= enteredNumber;
-
-   writeOutputLog("*", initialResult, enteredNumber);
-
-   writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+   calculateResult("MULTIPLY");
 }
 
 // function divide()
 function divide() {
-   const enteredNumber = getUserNumberInput();
-   const initialResult = currentResult;
-   currentResult /= enteredNumber;
-
-   writeOutputLog("/", initialResult, enteredNumber);
-
-   writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
+   calculateResult("DIVIDE");
 }
-
-// alert(result); result is not defined (out of scope)
-
-// const additionResult = add(1, 2);
-
-// currentResult = additionResult;
-
-// let calculationDescription = `${defaultResult} + ${currentResult}`;
-// let errorMessage = "An error \n" + "occurred!";
 
 addBtn.addEventListener("click", add);
 subtractBtn.addEventListener("click", subtract);
---
+++ javascript-complete-guide-2022/basics-01-starting-project/assets/scripts/utils.js
@@ -0,0 +1,61 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+// using arrays
+let logEntries = [];
+
+// get the user input
+function getUserNumberInput() {
+   // parseInt() converts string to number
+   return parseInt(userInput.value);
+}
+
+// generate and write calculation log
+function writeOutputLog(operator, resultBeforeCalc, calcNumber) {
+   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+   outputResult(currentResult, calcDescription); // from vendor.js
+}
+
+// function writeToLog
+function writeToLog(
+   operationIdentifier,
+   initialResult,
+   enteredNumber,
+   newResult
+) {
+   const logEntry = {
+      operation: operationIdentifier,
+      initialResult: initialResult,
+      enteredNumber: enteredNumber,
+      result: newResult,
+   };
+   logEntries.push(logEntry);
+   console.log(logEntries);
+}
+
+// calculate the result
+function calculateResult(calculationType) {
+   const enteredNumber = getUserNumberInput();
+   const initialResult = currentResult;
+   let mathOperator;
+
+   if (calculationType === "ADD") {
+      currentResult += enteredNumber;
+      mathOperator = "+";
+   } else if (calculationType === "SUBTRACT") {
+      currentResult -= enteredNumber;
+      mathOperator = "-";
+   } else if (calculationType === "MULTIPLY") {
+      currentResult *= enteredNumber;
+      mathOperator = "*";
+   } else if (calculationType === "DIVIDE") {
+      currentResult /= enteredNumber;
+      mathOperator = "/";
+   } else {
+      return Error(
+         "Hey you forgot to input operation (ADD/DIVIDE/MULTIPLY/DIVIDE)!"
+      );
+   }
+
+   writeOutputLog(mathOperator, initialResult, enteredNumber);
+   writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
