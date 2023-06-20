2a536835ac1d7022805001fe2eafe99c75913c01
nuhptr <nugrahaadi733@gmail.com>
2023-06-20 22:55:54 +0700

    programming paradigms

---
+++ javascript-complete-guide-2022/programming-paradigms/paradigms-practice/functional.js
@@ -0,0 +1,55 @@
+const REQUIRED = "REQUIRED";
+const MIN_LENGTH = "MIN_LENGTH";
+
+function validate(value, flag, validatorValue) {
+   if (flag === REQUIRED) {
+      return value.trim().length > 0;
+   }
+
+   if (flag === MIN_LENGTH) {
+      return value.trim().length > validatorValue;
+   }
+}
+
+function createUser(userName, password) {
+   if (!validate(userName, REQUIRED) || !validate(password, MIN_LENGTH, 5)) {
+      throw new Error(
+         "Invalid input - username or password is wrong (password should be at least six characters)."
+      );
+   }
+
+   return {
+      userName: userName,
+      password: password,
+   };
+}
+
+function greetUser(user) {
+   console.log(`Hi, I am ${user.userName}`);
+}
+
+function getUserInput(inputElementId) {
+   return document.getElementById(inputElementId).value;
+}
+
+function signUpHandler(event) {
+   event.preventDefault();
+
+   const enteredUserName = getUserInput("username");
+   const enteredPassword = getUserInput("password");
+
+   try {
+      const newUser = createUser(enteredUserName, enteredPassword);
+      console.log(newUser);
+      greetUser(newUser);
+   } catch (err) {
+      alert(err.message);
+   }
+}
+
+function connectForm(formId, formSubmitHandler) {
+   const form = document.getElementById(formId);
+   form.addEventListener("submit", formSubmitHandler);
+}
+
+connectForm("user-input", signUpHandler);
--- javascript-complete-guide-2022/programming-paradigms/paradigms-practice/oop.js
+++ javascript-complete-guide-2022/programming-paradigms/paradigms-practice/oop.js
@@ -1,6 +1,28 @@
-class Validator {}
+class Validator {
+   static REQUIRED = "REQUIRED";
+   static MIN_LENGTH = "MIN_LENGTH";
 
-class User {}
+   static validate(value, flag, validatorValue) {
+      if (flag === this.REQUIRED) {
+         return value.trim().length > 0;
+      }
+
+      if (flag === this.MIN_LENGTH) {
+         return value.trim().length > validatorValue;
+      }
+   }
+}
+
+class User {
+   constructor(uName, uPassword) {
+      this.userName = uName;
+      this.password = uPassword;
+   }
+
+   greet() {
+      console.log(`Hi, I am ${this.userName}`);
+   }
+}
 
 class UserInputForm {
    constructor() {
@@ -13,8 +35,23 @@ class UserInputForm {
 
    signUpHandler(event) {
       event.preventDefault();
-
       const enteredUserName = this.userNameInput.value;
       const enteredPassword = this.passwordInput.value;
+
+      if (
+         !Validator.validate(enteredUserName, Validator.REQUIRED) ||
+         !Validator.validate(enteredPassword, Validator.MIN_LENGTH, 5)
+      ) {
+         alert(
+            "Invalid input - username or password is wrong (password should be at least six characters)."
+         );
+         return;
+      }
+
+      const newUser = new User(enteredUserName, enteredPassword);
+      console.log(newUser);
+      newUser.greet();
    }
 }
+
+new UserInputForm();
