055ecddc053643ed294977ece5de29ccd1e1bb0f
nuhptr <nugrahaadi733@gmail.com>
2023-10-29 14:18:25 +0700

    update app and knowledge

--- javascript-complete-guide-2022/01 Basics/src/app.js
+++ javascript-complete-guide-2022/01 Basics/src/app.js
@@ -3,16 +3,18 @@ const defaultResult = 0
 let currentResult = defaultResult
 
 // function
-function add(num1, num2) {
-   const result = +num1 + +num2
-   return result
+function add() {
+   currentResult = currentResult + parseInt(userInput.value)
+   outputResult(currentResult, "")
+
+   // return result
 }
 
 // call function
-currentResult = add(5, 5)
+// currentResult = add(5, 5)
 // currentResult = currentResult + (10 * 2) / 2 - 1
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
+// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
 // let errorMessage = "An error \n" + "occurred!"
 
-outputResult(currentResult, calculationDescription)
+addBtn.addEventListener("click", add)
