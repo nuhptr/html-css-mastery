aaf3caa9b444ffdb536ded8287874e4f3cea32e8
nuhptr <nugrahaadi733@gmail.com>
2023-10-29 14:48:46 +0700

    basics done

--- javascript-complete-guide-2022/01 Basics/src/app.js
+++ javascript-complete-guide-2022/01 Basics/src/app.js
@@ -1,45 +1,63 @@
-// definingn variable
+// defining variable
 const defaultResult = 0
 let currentResult = defaultResult
 
+let logEntries = []
+
 // splitting function
+// get input from input field
 function getUserNumberInput() {
-   return parseInt(userInput.value)
+   return parseInt(userInput.value) // from vendor.js
+}
+
+// generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+   outputResult(currentResult, calcDescription) // from vendor.js
+}
+
+// push to array logEntries
+function writeToLog(operationType, prevResult, enteredNumber, newResult) {
+   const logEntry = { operation: operationType, prevResult: prevResult, number: enteredNumber, result: newResult }
+   logEntries.push(logEntry)
 }
 
 // function
 function add() {
    const enteredNumber = getUserNumberInput()
+   const initialResult = currentResult
 
-   const calcDescription = `${currentResult} + ${enteredNumber}`
-   currentResult = currentResult + enteredNumber
-   outputResult(currentResult, calcDescription)
-
+   currentResult += enteredNumber
+   createAndWriteOutput("+", initialResult, enteredNumber)
+   writeToLog("ADD", initialResult, enteredNumber, currentResult)
    // return result
 }
 
 function substraction() {
    const enteredNumber = getUserNumberInput()
+   const initialResult = currentResult
 
-   const calcDescription = `${currentResult} - ${enteredNumber}`
-   currentResult = currentResult - enteredNumber
-   outputResult(currentResult, calcDescription)
+   currentResult -= enteredNumber
+   createAndWriteOutput("-", initialResult, enteredNumber)
+   writeToLog("SUBSTRACT", initialResult, enteredNumber, currentResult)
 }
 
 function multiplication() {
    const enteredNumber = getUserNumberInput()
+   const initialResult = currentResult
 
-   const calcDescription = `${currentResult} * ${enteredNumber}`
-   currentResult = currentResult * enteredNumber
-   outputResult(currentResult, calcDescription)
+   currentResult *= enteredNumber
+   createAndWriteOutput("*", initialResult, enteredNumber)
+   writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult)
 }
 
 function division() {
    const enteredNumber = getUserNumberInput()
+   const initialResult = currentResult
 
-   const calcDescription = `${currentResult} / ${enteredNumber}`
-   currentResult = currentResult / enteredNumber
-   outputResult(currentResult, calcDescription)
+   currentResult /= enteredNumber
+   createAndWriteOutput("/", initialResult, enteredNumber)
+   writeToLog("DIVIDE", initialResult, enteredNumber, currentResult)
 }
 
 // call function
@@ -49,4 +67,7 @@ function division() {
 // let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
 // let errorMessage = "An error \n" + "occurred!"
 
-addBtn.addEventListener("click", add)
+addBtn.addEventListener("click", add) // from vendor.js
+subtractBtn.addEventListener("click", substraction) // from vendor.js
+multiplyBtn.addEventListener("click", multiplication) // from vendor.js
+divideBtn.addEventListener("click", division) // from vendor.js
