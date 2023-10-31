570d4993d4715871d99272e78a70bbc2cfaf130a
nuhptr <nugrahaadi733@gmail.com>
2023-10-31 18:44:51 +0700

    update path

--- Javascript - The Complete Guide 2023/06 Working With DOM/src/app.js
+++
@@ -1,8 +0,0 @@
-window.alert("Hello World!")
-
-//? WINDOW OBJECT
-console.dir(window.document)
-
-const h1 = document.querySelector("h1")
-const li2 = document.querySelector("li")[2]
-const list = document.querySelector("#list")
