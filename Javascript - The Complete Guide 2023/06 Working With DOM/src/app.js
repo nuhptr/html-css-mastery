8252c12526e44af11f0a069eddb166ace156fef3
nuhptr <nugrahaadi733@gmail.com>
2023-10-31 17:03:33 +0700

    update code

--- Javascript - The Complete Guide 2023/06 Working With DOM/src/app.js
+++ Javascript - The Complete Guide 2023/06 Working With DOM/src/app.js
@@ -2,3 +2,7 @@ window.alert("Hello World!")
 
 //? WINDOW OBJECT
 console.dir(window.document)
+
+const h1 = document.querySelector("h1")
+const li2 = document.querySelector("li")[2]
+const list = document.querySelector("#list")
