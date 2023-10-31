a334d827e07e153c3a9e860434ccf4da3ccfdd4f
nuhptr <nugrahaadi733@gmail.com>
2023-10-31 10:42:17 +0700

    update code

--- Javascript - The Complete Guide 2023/01 Basics/src/app.js
+++ Javascript - The Complete Guide 2023/01 Basics/src/app.js
@@ -1,28 +1,28 @@
-// defining variable
+//? DEFINING VARIABLES
 const defaultResult = 0
 let currentResult = defaultResult
 
 let logEntries = []
 
-// splitting function
-// get input from input field
+//? SPLITTING CODE INTO FUNCTIONS
+//? GET USER INPUT
 function getUserNumberInput() {
    return parseInt(userInput.value) // from vendor.js
 }
 
-// generates and writes calculation log
+//? GENERATE AND WRITE CALCULATION LOG
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
    outputResult(currentResult, calcDescription) // from vendor.js
 }
 
-// push to array logEntries
+//? WRITE TO LOG
 function writeToLog(operationType, prevResult, enteredNumber, newResult) {
    const logEntry = { operation: operationType, prevResult: prevResult, number: enteredNumber, result: newResult }
    logEntries.push(logEntry)
 }
 
-// function
+//? FUNCTION DECLARATION
 function add() {
    const enteredNumber = getUserNumberInput()
    const initialResult = currentResult
@@ -59,13 +59,6 @@ function division() {
    createAndWriteOutput("/", initialResult, enteredNumber)
    writeToLog("DIVIDE", initialResult, enteredNumber, currentResult)
 }
-
-// call function
-// currentResult = add(5, 5)
-// currentResult = currentResult + (10 * 2) / 2 - 1
-
-// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
-// let errorMessage = "An error \n" + "occurred!"
 
 addBtn.addEventListener("click", add) // from vendor.js
 subtractBtn.addEventListener("click", substraction) // from vendor.js
