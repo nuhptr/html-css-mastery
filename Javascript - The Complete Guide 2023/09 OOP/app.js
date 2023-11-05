e547e64ca20f23d8a4ad3b23e7afa7aa6af9befa
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-11-05 18:18:13 +0700

    object/app update code

--- Javascript - The Complete Guide 2023/09 OOP/app.js
+++ Javascript - The Complete Guide 2023/09 OOP/app.js
@@ -1,41 +1,73 @@
-const productList = {
-   products: [
-      {
-         name: "A Pillow",
-         imageURL: "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
-         price: 1000,
-         description: "This is a soft pillow!",
-      },
-      {
-         name: "A Carpet",
-         imageURL: "https://www.publicdomainpictures.net/pictures/280000/velka/plain-red-background.jpg",
-         price: 2000,
-         description: "This is a carpet!",
-      },
-   ],
+class Product {
+   constructor(title = "DEFAULT", image = "", desc = "", price = 0) {
+      this.title = title
+      this.imageURL = image
+      this.description = desc
+      this.price = price
+   }
+
+   getData() {
+      return { title: this.title, image: this.imageURL, description: this.description, price: this.price }
+   }
+}
+
+class ProductItem {
+   constructor(product) {
+      this.product = product
+   }
+
+   render() {
+      const prodEl = document.createElement("li")
+      prodEl.className = "product-item"
+      prodEl.innerHTML = `
+                <div>
+                 <img src="${this.product.imageURL}" alt="${this.product.name}">
+                 <div class="product-item__content">
+                    <h2>${this.product.name}</h2>
+                    <h3>${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                    <button>Add to Cart</button>
+                 </div>
+                </div>
+             `
+      return prodEl
+   }
+}
+
+class ProductList {
+   products = [
+      new Product(
+         "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
+         "This is a soft pillow!",
+         19.99,
+         "A Pillow"
+      ),
+      new Product(
+         "A Carpet",
+         "https://www.publicdomainpictures.net/pictures/280000/velka/plain-red-background.jpg",
+         "This is a carpet!",
+         89.99
+      ),
+   ]
+
+   // constructor() {}
+
    render() {
       const renderHook = document.getElementById("app")
       const prodList = document.createElement("ul")
 
       prodList.className = "product-list"
       for (const prod of this.products) {
-         const prodEl = document.createElement("li")
-         prodEl.className = "product-item"
-         prodEl.innerHTML = `
-                <div>
-                 <img src="${prod.imageURL}" alt="${prod.name}">
-                 <div class="product-item__content">
-                    <h2>${prod.name}</h2>
-                    <h3>${prod.price}</h3>
-                    <p>${prod.description}</p>
-                    <button>Add to Cart</button>
-                 </div>
-                </div>
-             `
+         //? CALL THE CONSTRUCTOR OF ProductItem CLASS
+         const productItem = new ProductItem(prod)
+         const prodEl = productItem.render()
+
          prodList.append(prodEl)
       }
+
       renderHook.append(prodList)
-   },
+   }
 }
 
+const productList = new ProductList()
 productList.render()
