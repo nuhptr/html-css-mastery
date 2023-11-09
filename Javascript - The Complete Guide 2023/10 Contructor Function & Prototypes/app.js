6513b27df74370e2b2e0970d70d5a50ae417ca8f
nuhptr <nugrahaadi733@gmail.com>
2023-11-09 11:34:20 +0700

    app/update code

--- Javascript - The Complete Guide 2023/10 Contructor Function & Prototypes/app.js
+++ Javascript - The Complete Guide 2023/10 Contructor Function & Prototypes/app.js
@@ -1,26 +1,21 @@
-//? LAST LEARN (OOP)
-// class Person {
-//    name = "Max"
-
-//    constructor() {
-//       this.age = 30
-//    }
-
-//    greet() {
-//       console.log(`Hi, I am ${this.name} and I am ${this.age} years old.`)
-//    }
-// }
-
 function Person() {
    this.age = 30
    this.name = "Max"
    this.greet = () => console.log(`Hi, I am ${this.name} and I am ${this.age} years old.`)
 }
 
-Person.prototype = {
-   printAge() {
-      console.log(this.age)
-   },
+// Person.prototype = {
+//    printAge() {
+//       console.log(this.age)
+//    },
+// }
+
+Person.describe = function () {
+   console.log("Creating persons...")
+}
+
+Person.prototype.printAge = function () {
+   console.log(this.age)
 }
 
 console.dir(Person)
@@ -29,4 +24,9 @@ const p = new Person()
 p.greet() // Hi, I am Max and I am 30 years old.
 p.printAge() // 30
 
-console.log(p.__proto__)
+console.log(p.length) // undefined
+console.log(p.toString())
+
+const p2 = new p.__proto__.constructor()
+console.log(p2) // Person {age: 30, name: "Max", greet: ƒ}
+console.dir(Object.prototype.__proto__) // null
