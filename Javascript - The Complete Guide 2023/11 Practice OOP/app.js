b3095029f77d71c8941ea5a27425c47e402feef7
nuhptr <nugrahaadi733@gmail.com>
2023-11-14 18:43:52 +0700

    pratice-oop/app update

--- Javascript - The Complete Guide 2023/11 Practice OOP/app.js
+++ Javascript - The Complete Guide 2023/11 Practice OOP/app.js
@@ -1,8 +1,9 @@
 class Tooltip {}
 
 class ProjectItem {
-   constructor(id) {
+   constructor(id, updateProjectListsFunction) {
       this.id = id
+      this.updateProjectListsHandler = updateProjectListsFunction
       this.connectMoreInfoButton()
       this.connectSwitchButton()
    }
@@ -12,6 +13,7 @@ class ProjectItem {
    connectSwitchButton() {
       const projectItemElement = document.getElementById(this.id)
       const switchBtn = projectItemElement.querySelector("button:last-of-type")
+      switchBtn.addEventListener("click", this.updateProjectListsHandler)
    }
 }
 
@@ -19,11 +21,25 @@ class ProjectList {
    projects = []
 
    constructor(type) {
-      const prjItems = document.querySelectorAll(`#${type}-projects li`)
-      for (const prjItem of prjItems) {
-         this.projects.push(new ProjectItem(prjItem.id))
+      this.type = type
+      const projectItems = document.querySelectorAll(`#${type}-projects li`)
+
+      for (const projectItem of projectItems) {
+         this.projects.push(new ProjectItem(projectItem.id, this.switchProject.bind(this)))
       }
-      console.log(this.projects)
+   }
+
+   setSwitchHandlerFunction(switchHandlerFunction) {
+      this.switchHandler = switchHandlerFunction
+   }
+
+   addProject() {
+      console.log(this)
+   }
+
+   switchProject(projectId) {
+      this.switchHandler(this.projects.find((p) => p.id === projectId))
+      this.projects = this.projects.filter((p) => p.id !== projectId)
    }
 }
 
@@ -31,6 +47,9 @@ class App {
    static init() {
       const activeProjectsList = new ProjectList("active")
       const finishedProjectsList = new ProjectList("finished")
+
+      activeProjectsList.setSwitchHandlerFunction(finishedProjectsList.addProject.bind(finishedProjectsList))
+      finishedProjectsList.setSwitchHandlerFunction(activeProjectsList.addProject.bind(activeProjectsList))
    }
 }
 
