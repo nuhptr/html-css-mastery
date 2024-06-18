bc367adb48757cde5aaa0066699f34f5286b9732
nuhptr <nugrahaadi733@gmail.com>
2024-06-18 18:43:17 +0700

    refactor(modern-html-scope): :art: update global formatting

--- modern-html-css-sass/intermediate-keyframe-transition-etc-project/timeline-knowledge/main.js
+++ modern-html-css-sass/intermediate-keyframe-transition-etc-project/timeline-knowledge/main.js
@@ -1,25 +1,24 @@
-const items = document.querySelectorAll(`#timeline li`);
+const items = document.querySelectorAll(`#timeline li`)
 
 const inViewPort = (el) => {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
+   const rect = el.getBoundingClientRect()
+   return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+   )
+}
 
 const run = () => {
-  items.forEach((item) => {
-    if (inViewPort(item)) {
-      item.classList.add("show");
-    }
-  });
-};
+   items.forEach((item) => {
+      if (inViewPort(item)) {
+         item.classList.add("show")
+      }
+   })
+}
 
 // Events
-window.addEventListener("load", run);
-window.addEventListener("resize", run);
-window.addEventListener("scroll", run);
+window.addEventListener("load", run)
+window.addEventListener("resize", run)
+window.addEventListener("scroll", run)
